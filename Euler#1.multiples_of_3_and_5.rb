a06db8c0f0e6f8ff76d66a075ee399c0a047737e
Emp-Norton <jamesjelenko@gmail.com>
2017-04-26 17:26:05 -0700

    Update Euler#1.multiples_of_3_and_5.rb

--- Euler#1.multiples_of_3_and_5.rb
+++ Euler#1.multiples_of_3_and_5.rb
@@ -1,6 +1,6 @@
 def multiples(n)
   sum = 0
-  (0...n).each { | i | sum += i if (i%3).eql?(0) || (i%5).eql?(0) }
+  (0...n).each { | i | sum += i if (i % 3).eql?(0) || (i % 5).eql?(0) }
   return sum 
 end 
 
