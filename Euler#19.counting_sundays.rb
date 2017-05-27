dedc9b3c1b7509f2a8f4951cd5bd410c18c1d41e
Emp-Norton <jamesjelenko@gmail.com>
2017-05-27 14:16:48 -0700

    Update Euler#19.counting_sundays.rb

--- Euler#19.counting_sundays.rb
+++ Euler#19.counting_sundays.rb
@@ -1,13 +1,13 @@
 year = 1901 
-months = [31,28,31,30,31,30,31,31,30,31,30,31]
+days_per_month = [31,28,31,30,31,30,31,31,30,31,30,31]
 total_sundays = 0 
 day_of_week = 2 # tuesday 
 
-while year < 2001 
+while year <= 2000
 
-  (year % 4).eql?(0) && (year.to_s.slice(year.to_s.length-2, 2) != "00") ? months[1] = 29 : months[1] = 28  # leap year handler. Oly works for given range. Centuries divisible by 400 will be missed e.g. 1600 / 2000 / 2400. 
+  (year % 4).eql?(0) && (year.to_s.slice(year.to_s.length-2, 2) != "00") ? days_per_month[1] = 29 : days_per_month[1] = 28  # leap year handler. Oly works for given range. Centuries divisible by 400 will be missed e.g. 1600 / 2000 / 2400. 
 
-  months.each do | month_length | 
+  days_per_month.each do | month_length | 
     for day_of_month in 1..month_length 
     day_of_week = day_of_week % 7 
       if day_of_week.eql?(0) && day_of_month.eql?(1)
